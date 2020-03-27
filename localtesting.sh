114cc8b6b34d7852f921d9a124607a7d1ee10df1
Hakan Kaynar <hakankaynar@Hakans-MacBook-Air.local>
2020-03-27 12:42:56 +0100

    Added localtesting.sh

---
+++ localtesting.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+NO_DOCKER="false"
+
+if [ "$1" = "no-docker" ]; then
+  NO_DOCKER="true"
+fi
+
+echo "Setting up virtial environment"
+python3 -m venv venv && source venv/bin/activate
+
+echo "Installing requirements"
+pip install -r requirements.txt
+
+if [ "$NO_DOCKER" = "false" ]; then
+  echo "Creating docker elastic search network"
+  docker network create elastic-search
+
+  echo "Running elastic search"
+  docker run --rm -d --name elastic-search --net elastic-search -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.6.1
+
+  echo "Waiting 30 seconds"
+  sleep 30
+fi
+
+echo "Configuring python path"
+export PYTHONPATH=${PYTHONPATH}:.
+
+echo "Running the tests"
+python -m coverage run -m unittest discover './test' '*_test.py'
+
+echo "Reporting coverage"
+python -m coverage report --include **/product/*.py  --fail-under 90
+
+if [ "$NO_DOCKER" = "false" ]; then
+  echo "Stopping elastic search"
+  docker container stop elastic-search
+fi
